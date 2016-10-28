77c3c85852798d046a29dcd42ac73585769f4759
Jeremy Dreher <jeremyadreher@gmail.com>
2016-10-28 16:28:59 -0700

    Second Commit - Added to Design section

---
+++ dropDown.js
@@ -0,0 +1,29 @@
+/*
+$(document).ready(function() {
+    $("#loc_1").hide();
+    $("#loc_2").hide();
+    $("#loc_3").hide();
+    $("#loc_4").hide();
+
+$("#locMenu").hover(function() {
+    $("#loc_1").slideToggle();
+    $("#loc_2").slideToggle();
+    $("#loc_3").slideToggle();
+    $("#loc_4").slideToggle();
+}); 
+
+});
+*/
+
+
+$(document).ready(function() {
+    $(".loc").hide();
+
+$("#locMenu").hover(function() {
+    $(".loc").slideToggle();
+}); 
+
+});
+
+
+
